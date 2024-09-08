b303d1eed50021b79a6e2ed8256cf4f0d323cfb0
Amshumankrishna <amshumankrishna@gmail.com>
2024-09-09 00:02:32 +0530

    commit

--- script.js
+++
@@ -1,5 +0,0 @@
-// Add a dynamic skills bar or any other interactivity you like
-document.addEventListener('DOMContentLoaded', () => {
-    // Example: Console log when page is loaded
-    console.log("Portfolio page loaded");
-});
